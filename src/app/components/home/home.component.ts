ad9ef1c31f7a1f55dd072f4504c3eca85efed1cd
Cesar Bermudez <cesar.bermudez.sierra@gmail.com>
2022-07-14 01:48:23 -0500

    Se agrega reproductor y seleccion para reproducir audio

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,55 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ViewChild, ElementRef } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  @ViewChild("musicInput") music!: ElementRef;
+  public src: any;
+  public reproductor: any;
+  public nameSong!: string;
+  public dvd = "../../../assets/dvd.gif"
+  constructor(private domSanitizer: DomSanitizer) { }
 
   ngOnInit(): void {
+    this.reproductor = <HTMLAudioElement>document.getElementById("reproductor")
+  }
+
+  selectSong(event: any) {
+    const type = event.target.files[0].type;
+    if (type !== "audio/mpeg") {
+      alert("Seleccione un archivo mp3");
+      this.music.nativeElement.value = null;
+      return;
+    }
+    this.nameSong = event.target.files[0].name;
+    this.audioToBase64(event.target.files[0])
+      .then((res: any) => {
+        this.src = this.domSanitizer.bypassSecurityTrustUrl(res);
+      });
+    setTimeout(() => {
+      this.play();
+    }, 1000);
+  }
+
+  play() {
+    this.reproductor.play();
+  }
+
+  stop() {
+    this.reproductor.pause()
+  }
+
+  async audioToBase64(audioFile: any) {
+    return new Promise((resolve, reject) => {
+      let reader = new FileReader();
+      reader.onerror = reject;
+      reader.onload = (e) => resolve(e?.target?.result);
+      reader.readAsDataURL(audioFile);
+    });
   }
 
 }
